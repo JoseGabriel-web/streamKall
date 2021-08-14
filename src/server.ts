cd9040d29a25a99a566a7e772ff79888420fc62e
Jose Gabriel Mercedes Geronimo <josegabriel.mercedes@yahoo.com>
2021-08-14 19:58:40 -0400

    test deploy to heroku

--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,24 @@ import http from "http";
 import https from "https";
 import { enviroment, port } from "./config";
 
-if (enviroment === "production") {
-  const httpsServer = https.createServer({}, app);
-  socketIoConfig(httpsServer);
+// if (enviroment === "production") {
+//   const httpsServer = https.createServer({}, app);
+//   socketIoConfig(httpsServer);
 
-  httpsServer.listen(port, () => {
-    console.log(`HTTPS server running on port ${port}`);
-  });
-} else {
-  const httpServer = http.createServer({}, app);
-  socketIoConfig(httpServer);
+//   httpsServer.listen(port, () => {
+//     console.log(`HTTPS server running on port ${port}`);
+//   });
+// } else {
+//   const httpServer = http.createServer({}, app);
+//   socketIoConfig(httpServer);
 
-  httpServer.listen(port, () => {
-    console.log(`HTTP server running on port ${port}`);
-  });
-}
+//   httpServer.listen(port, () => {
+//     console.log(`HTTP server running on port ${port}`);
+//   });
+// }
+const httpServer = http.createServer({}, app);
+socketIoConfig(httpServer);
+
+httpServer.listen(port, () => {
+  console.log(`HTTP server running on port ${port}`);
+});
